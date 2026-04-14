46ff7ade24a724b61769934c3bb36a7bfdb66e5e
neidy254 <neidydev@gmail.com>
2026-04-14 20:16:10 +0700

    prepare to submit

--- test/runtests.jl
+++ test/runtests.jl
@@ -39,6 +39,7 @@ end
 
     test_cases = [
         ("data/toy/toy_data.txt", 3, "data/spmf/toy_spmf.txt"),
+        ("data/benchmark/mushroom.txt", 2000, "data/spmf/mushroom_spmf.txt"),
         ("data/benchmark/chess.txt", 2500, "data/spmf/chess_spmf.txt"), ("data/benchmark/retail.txt", 997, "data/spmf/retail_spmf.txt"),
         ("data/benchmark/accident.txt", 199994, "data/spmf/accident_spmf.txt")
     ]
